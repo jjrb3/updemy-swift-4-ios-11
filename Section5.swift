4a1afaf65d1c90a0d2b396d9e31b61de3268e2e6
= <jjrb6@hotmail.com>
2019-02-10 13:52:49 -0500

    Added type character

--- Section5.swift
+++ Section5.swift
@@ -1,9 +1,10 @@
 import Foundation 
 
-let name    : String = "Jeremy Reyes" // Declaración explicita
-let number  : Int    = 123456789     
-let pi      : Double = 3.1416         // Consume menos memoria
-let piFloat : Float  = 3.1416
-let isTrue  : Bool   = true
+let name    : String 	= "Jeremy Reyes" // Declaración explicita
+let number  : Int    	= 123456789     
+let pi      : Double 	= 3.1416         // Consume menos memoria
+let piFloat : Float  	= 3.1416
+let isTrue  : Bool   	= true
+let char	: Character = "A"
 
 print(name, number, pi, piFloat, isTrue)
